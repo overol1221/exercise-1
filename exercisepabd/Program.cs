4e69495b37860534a72dd0d9fc1d871ad02be5f9
overol1221 <fiqiachmad12@gmail.com>
2023-04-05 10:19:11 +0700

    Create SqlConnection and make a login for connection to database

--- exercisepabd/Program.cs
+++ exercisepabd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,40 @@ namespace exercisepabd
     {
         static void Main(string[] args)
         {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi ke Database\n");
+                    Console.WriteLine("Masukkan User ID :");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan database tujuan");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk Terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    {
+                        case 'K':
+                            {
+                                SqlConnection conn = null;
+                                string strKoneksi = "Data source = MAD\\MADDOG12; " +
+                                "initial catalog = {0}; " + "User ID {1}; password = {2}";
+                                conn = new SqlConnection(string.Format(strKoneksi, db, user, pass));
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                {
+
+                                }
+                            }
+                    }
+                    
+                }
+
+            }
         }
     }
 }
